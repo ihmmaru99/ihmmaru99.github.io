4ce4dcaf68dc7814e598c3b3d2b56aa8c787fd35
ihmmaru99 <ihmmaru99@gmail.com>
2023-05-26 19:52:27 +0900

    test

--- _pages/category-archive.md
+++ _pages/category-archive.md
@@ -4,8 +4,4 @@ title: "Category"
 layout: categories
 author_profile: true
 sidebar_main: true
-toc: true
-toc_sticky: true
-toc_label: "Categories"
 ---
-카테고리
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
+        "title": "Test2",
+        "excerpt":"원래 힘으로 돌아가려는 힘 벡터 = \\(\\vec{f_{ij,spring}} = -k(\\vert\\vec{x_i} - \\vec{x_j}\\vert - l_0){\\vec{x_i} - \\vec{x_j}\\over{\\vert\\vec{x_i} - \\vec{x_j}\\vert}} \\)   ","categories": ["Cpp"],
+        "tags": ["Cpp"],
+        "url": "/cpp/test2/",
+        "teaser": null
+      },{
         "title": "Test",
-        "excerpt":"원래 힘으로 돌아가려는 힘 벡터 = \\(\\vec{f_{ij,spring}} = -k(\\vert\\vec{x_i} - \\vec{x_j}\\vert - l_0){\\vec{x_i} - \\vec{x_j}\\over{\\vert\\vec{x_i} - \\vec{x_j}\\vert}} \\)   ","categories": ["Blog"],
-        "tags": ["Blog","jekyll","Github","Git"],
-        "url": "/blog/test/",
+        "excerpt":"원래 힘으로 돌아가려는 힘 벡터 = \\(\\vec{f_{ij,spring}} = -k(\\vert\\vec{x_i} - \\vec{x_j}\\vert - l_0){\\vec{x_i} - \\vec{x_j}\\over{\\vert\\vec{x_i} - \\vec{x_j}\\vert}} \\)   ","categories": ["Cpp"],
+        "tags": ["Cpp"],
+        "url": "/cpp/test/",
         "teaser": null
       }]
