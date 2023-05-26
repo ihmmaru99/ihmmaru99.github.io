7d57222ee76255f71a71daf090f8489b5dbb3863
ihmmaru99 <ihmmaru99@gmail.com>
2023-05-26 10:38:22 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Test",
+        "excerpt":" ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/test/",
+        "teaser": null
+      }]
