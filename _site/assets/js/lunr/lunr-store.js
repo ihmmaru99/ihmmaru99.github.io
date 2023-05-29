785f647c861789133ecf6d0eecd2911ca48ef022
ihmmaru99 <ihmmaru99@gmail.com>
2023-05-29 19:36:49 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,8 +5,8 @@ var store = [{
         "url": "/cpp/test2/",
         "teaser": null
       },{
-        "title": "Test",
-        "excerpt":"원래 힘으로 돌아가려는 힘 벡터 = \\(\\vec{f_{ij,spring}} = -k(\\vert\\vec{x_i} - \\vec{x_j}\\vert - l_0){\\vec{x_i} - \\vec{x_j}\\over{\\vert\\vec{x_i} - \\vec{x_j}\\vert}} \\)   ","categories": ["Cpp"],
+        "title": "[C++] 테스트입니다.",
+        "excerpt":"임정빈           세종과학고 8기            한양대학교 미래자동차공학과 18학번                      반갑습니다.                   #include &lt;iostream&gt; using namespace std; int main(){   cout &lt;&lt; \"Hello World\" &lt;&lt; '\\n'; }  https://ihmmaru99.github.io      철은 무거우니까 들지 말자         어차피 인생은 굴러먹다가 가는 뜬구름 같은          ","categories": ["Cpp"],
         "tags": ["Cpp"],
         "url": "/cpp/test/",
         "teaser": null
