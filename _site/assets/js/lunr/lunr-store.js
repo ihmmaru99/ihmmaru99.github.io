56645eb7f6e71cdd814180f0139b2f9cfe1e1ed7
ihmmaru99 <ihmmaru99@gmail.com>
2023-05-30 00:49:52 +0900

    test

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,19 @@
 var store = [{
         "title": "[C++] 테스트입니다.",
-        "excerpt":"임정빈   세종과학고 8기  #include &lt;iostream&gt; using namespace std; int main(){   cout &lt;&lt; \"Hello World\" &lt;&lt; '\\n'; }  한양대학교 미래자동차공학과 18학번  반갑꼬리      철은 무거우니까 들지 말자       ","categories": ["Cpp"],
+        "excerpt":"임정빈   세종과학고 8기  #include &lt;iostream&gt; using namespace std; int main(){   cout &lt;&lt; \"Hello World\" &lt;&lt; '\\n'; }  한양대학교 미래자동차공학과 18학번  반갑꼬리      철은 무거우니까 들지 말자       ","categories": ["Algorithm"],
         "tags": ["Cpp"],
-        "url": "/cpp/test/",
-        "teaser": null
-      },{
-        "title": "[C++] 테스트입니다",
-        "excerpt":"임정빈   세종과학고 8기  #include &lt;iostream&gt; using namespace std; int main(){   cout &lt;&lt; \"Hello World\" &lt;&lt; '\\n'; }  한양대학교 미래자동차공학과 18학번  반갑꾸릉내      어차피 인생은 굴러먹다가 가는 뜬구름 같은    ","categories": ["Algorithm"],
-        "tags": ["BOJ","Programmers"],
         "url": "/algorithm/test/",
         "teaser": null
       },{
         "title": "[C++] 테스트입니다",
-        "excerpt":"임정빈   세종과학고 8기  #include &lt;iostream&gt; using namespace std; int main(){   cout &lt;&lt; \"Hello World\" &lt;&lt; '\\n'; }  한양대학교 미래자동차공학과 18학번  반갑꾸릉내      어차피 인생은 굴러먹다가 가는 뜬구름 같은    ","categories": ["Algorithm"],
+        "excerpt":"임정빈   세종과학고 8기  #include &lt;iostream&gt; using namespace std; int main(){   cout &lt;&lt; \"Hello World\" &lt;&lt; '\\n'; }  한양대학교 미래자동차공학과 18학번  반갑꾸릉내      어차피 인생은 굴러먹다가 가는 뜬구름 같은    ","categories": ["BOJ"],
         "tags": ["BOJ","Programmers"],
-        "url": "/algorithm/test2/",
+        "url": "/boj/test/",
+        "teaser": null
+      },{
+        "title": "[C++] 테스트입니다",
+        "excerpt":"임정빈   세종과학고 8기  #include &lt;iostream&gt; using namespace std; int main(){   cout &lt;&lt; \"Hello World\" &lt;&lt; '\\n'; }  한양대학교 미래자동차공학과 18학번  반갑꾸릉내      어차피 인생은 굴러먹다가 가는 뜬구름 같은    ","categories": ["Programmers"],
+        "tags": ["BOJ","Programmers"],
+        "url": "/programmers/test2/",
         "teaser": null
       }]
