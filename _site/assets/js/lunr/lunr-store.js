d60ef143692cf3dd66b1ad6ce18059eada5034cf
ihmmaru99 <ihmmaru99@gmail.com>
2023-06-04 19:43:31 +0900

    modified

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -11,33 +11,39 @@ var store = [{
         "url": "/boj/13305/",
         "teaser": null
       },{
-        "title": "[백준][1946] 신입 사원 (작성 중)",
-        "excerpt":"신입 사원 난이도 실버 1 문제 입력 출력 결과 1946 - 신입 사원 #include &lt;iostream&gt; #include &lt;vector&gt; #include &lt;algorithm&gt; #include &lt;cstring&gt; using namespace std; int t, n; vector&lt;pair&lt;int,int&gt;&gt; v; int main(){ cin &gt;&gt; t; for(int i=0; i&lt;t; i++){ cin &gt;&gt; n; for(int j=0; j&lt;n; j++){ int a, b; cin &gt;&gt;...","categories": ["BOJ"],
+        "title": "[백준][1946] 신입 사원",
+        "excerpt":"신입 사원 난이도 실버 1 문제 언제나 최고만을 지향하는 굴지의 대기업 진영 주식회사가 신규 사원 채용을 실시한다. 인재 선발 시험은 1차 서류심사와 2차 면접시험으로 이루어진다. 최고만을 지향한다는 기업의 이념에 따라 그들은 최고의 인재들만을 사원으로 선발하고 싶어 한다. 그래서 진영 주식회사는, 다른 모든 지원자와 비교했을 때 서류심사 성적과 면접시험 성적 중...","categories": ["BOJ"],
         "tags": ["Algorithm","Coding Test","C++"],
         "url": "/boj/1946/",
         "teaser": null
       },{
-        "title": "[백준][2468] 안전 영역 (작성 중)",
+        "title": "[백준][2468] 안전 영역",
         "excerpt":"안전 영역 난이도 실버 1 문제 재난방재청에서는 많은 비가 내리는 장마철에 대비해서 다음과 같은 일을 계획하고 있다. 먼저 어떤 지역의 높이 정보를 파악한다. 그 다음에 그 지역에 많은 비가 내렸을 때 물에 잠기지 않는 안전한 영역이 최대로 몇 개가 만들어 지는 지를 조사하려고 한다. 이때, 문제를 간단하게 하기 위하여, 장마철에...","categories": ["BOJ"],
         "tags": ["Algorithm","Coding Test","C++"],
         "url": "/boj/2468/",
         "teaser": null
       },{
-        "title": "[백준][1149] RGB 거리 (작성 중)",
+        "title": "[백준][1149] RGB 거리",
         "excerpt":"RGB 거리 난이도 실버 1 문제 RGB거리에는 집이 N개 있다. 거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다. 집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다. 각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때, 아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자. 1번 집의...","categories": ["BOJ"],
         "tags": ["Algorithm","Coding Test","C++"],
         "url": "/boj/1149/",
         "teaser": null
       },{
-        "title": "[백준][7562] 나이트의 이동 (작성 중)",
+        "title": "[백준][7562] 나이트의 이동",
         "excerpt":"나이트의 이동 난이도 실버 1 문제 체스판 위에 한 나이트가 놓여져 있다. 나이트가 한 번에 이동할 수 있는 칸은 아래 그림에 나와있다. 나이트가 이동하려고 하는 칸이 주어진다. 나이트는 몇 번 움직이면 이 칸으로 이동할 수 있을까? 입력 입력의 첫째 줄에는 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 세 줄로 이루어져...","categories": ["BOJ"],
         "tags": ["Algorithm","Coding Test","C++"],
         "url": "/boj/7562/",
         "teaser": null
       },{
-        "title": "[백준][2583] 영역 구하기 (작성 중)",
+        "title": "[백준][2583] 영역 구하기",
         "excerpt":"영역 구하기 난이도 실버 1 문제 눈금의 간격이 1인 M×N(M,N≤100)크기의 모눈종이가 있다. 이 모눈종이 위에 눈금에 맞추어 K개의 직사각형을 그릴 때, 이들 K개의 직사각형의 내부를 제외한 나머지 부분이 몇 개의 분리된 영역으로 나누어진다. 예를 들어 M=5, N=7 인 모눈종이 위에 &lt;그림 1&gt;과 같이 직사각형 3개를 그렸다면, 그 나머지 영역은 &lt;그림...","categories": ["BOJ"],
         "tags": ["Algorithm","Coding Test","C++"],
         "url": "/boj/2583/",
+        "teaser": null
+      },{
+        "title": "[백준][16953] A → B",
+        "excerpt":"A → B 난이도 실버 2 문제 정수 A를 B로 바꾸려고 한다. 가능한 연산은 다음과 같은 두 가지이다. 2를 곱한다. 1을 수의 가장 오른쪽에 추가한다. A를 B로 바꾸는데 필요한 연산의 최솟값을 구해보자. 입력 첫째 줄에 A, B (1 ≤ A &lt; B ≤ \\(10^9\\))가 주어진다. 출력 A를 B로 바꾸는데 필요한...","categories": ["BOJ"],
+        "tags": ["Algorithm","Coding Test","C++"],
+        "url": "/boj/16953/",
         "teaser": null
       }]
